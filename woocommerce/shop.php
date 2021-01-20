b2d0dc985774080ad54ff387f4554abd94e7b5c0
Jonatan <storlundjonatan@gmail.com>
2021-01-20 22:22:32 +0200

    remove sorting and reset variations button from shop page

--- woocommerce/shop.php
+++ woocommerce/shop.php
@@ -1,7 +1,16 @@
 <?php
-add_action( 'woocommerce_before_shop_loop', 'handsome_bearded_guy_select_variations' );
+add_action( 'woocommerce_before_shop_loop', 'add_variations_in_shop_page' );
 
-function handsome_bearded_guy_select_variations() {
+function add_variations_in_shop_page() {
 	remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
 	add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_single_add_to_cart', 30 );
 }
+
+add_action( 'wp', 'remove_default_sorting_storefront' );
+  
+function remove_default_sorting_storefront() {
+   remove_action( 'woocommerce_after_shop_loop', 'woocommerce_catalog_ordering', 10 );
+   remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 10 );
+}
+
+add_filter('woocommerce_reset_variations_link', '__return_empty_string');
