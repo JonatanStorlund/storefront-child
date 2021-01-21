b0c5c5c8dadd038c1c2a435836d8ed2e6a4f5b3c
Jonatan <storlundjonatan@gmail.com>
2021-01-21 09:51:41 +0200

    remove variation prices

--- woocommerce/shop.php
+++ woocommerce/shop.php
@@ -14,3 +14,42 @@ function remove_default_sorting_storefront() {
 }
 
 add_filter('woocommerce_reset_variations_link', '__return_empty_string');
+
+//Hide Price Range for WooCommerce Variable Products
+add_filter( 'woocommerce_variable_sale_price_html', 
+'lw_variable_product_price', 10, 2 );
+add_filter( 'woocommerce_variable_price_html', 
+'lw_variable_product_price', 10, 2 );
+
+function lw_variable_product_price( $v_price, $v_product ) {
+
+// Product Price
+$prod_prices = array( $v_product->get_variation_price( 'min', true ), 
+$v_product->get_variation_price( 'max', true ) );
+$prod_price = $prod_prices[0]!==$prod_prices[1] ? sprintf(__('From: %1$s', 'woocommerce'), 
+wc_price( $prod_prices[0] ) ) : wc_price( $prod_prices[0] );
+
+// Regular Price
+$regular_prices = array( $v_product->get_variation_regular_price( 'min', true ), 
+$v_product->get_variation_regular_price( 'max', true ) );
+sort( $regular_prices );
+$regular_price = $regular_prices[0]!==$regular_prices[1] ? sprintf(__('From: %1$s','woocommerce')
+, wc_price( $regular_prices[0] ) ) : wc_price( $regular_prices[0] );
+
+if ( $prod_price !== $regular_price ) {
+$prod_price = '<del>'.$regular_price.$v_product->get_price_suffix() . '</del> <ins>' . 
+$prod_price . $v_product->get_price_suffix() . '</ins>';
+}
+return $prod_price;
+}
+
+//Hide “From:$X”
+add_filter('woocommerce_get_price_html', 'lw_hide_variation_price', 10, 2);
+function lw_hide_variation_price( $v_price, $v_product ) {
+$v_product_types = array( 'variable');
+if ( in_array ( $v_product->product_type, $v_product_types )) {
+return '';
+}
+// return regular price
+return $v_price;
+}
