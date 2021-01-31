94d58d14420809869ca35f2aaf3b47b03a5a2538
Jonatan <storlundjonatan@gmail.com>
2021-01-31 21:17:51 +0200

    fix ajax not working on simple products

--- woocommerce/shop.php
+++ woocommerce/shop.php
@@ -2,7 +2,6 @@
 add_action( 'woocommerce_before_shop_loop', 'add_variations_in_shop_page' );
 
 function add_variations_in_shop_page() {
-	remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
 	add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_single_add_to_cart', 30 );
 }
 
