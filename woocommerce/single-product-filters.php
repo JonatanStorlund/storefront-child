c66ccfc25665c1a570cb0d2d3fd11e4c2e7b3547
Jonatan <storlundjonatan@gmail.com>
2021-02-01 11:45:52 +0200

    fix macro display on single pages

--- woocommerce/single-product-filters.php
+++ woocommerce/single-product-filters.php
@@ -3,12 +3,14 @@
    if ( !is_shop() ) {
      global $product;
 
-     $post_post_excerpt = "<div class='wp-block-pb-accordion-item c-accordion__item js-accordion-item' data-initially-open='false' data-click-to-close='true' data-auto-close='true' data-scroll='false' data-scroll-offset='0'>
-     <h3 id='at-67843' class='c-accordion__title js-accordion-controller' role='button' tabindex='0' aria-controls='ac-67843' aria-expanded='false'>"
-     . pll__('More Details', 'sijomealprep') .
-     "</h3>
-     <div id='ac-67843' class='c-accordion__content' hidden='hidden'>$product->short_description</div>
-     </div>";
+     if ($product->short_description) {
+       $post_post_excerpt = "<div class='wp-block-pb-accordion-item c-accordion__item js-accordion-item' data-initially-open='false' data-click-to-close='true' data-auto-close='true' data-scroll='false' data-scroll-offset='0'>
+       <h3 id='at-67843' class='c-accordion__title js-accordion-controller' role='button' tabindex='0' aria-controls='ac-67843' aria-expanded='false'>"
+       . pll__('More Details', 'sijomealprep') .
+       "</h3>
+       <div id='ac-67843' class='c-accordion__content' hidden='hidden'>$product->short_description</div>
+       </div>";
+     }
    }
 
   return $post_post_excerpt;
@@ -38,26 +40,30 @@ function sv_remove_product_page_skus( $enabled ) {
 }
 add_filter( 'wc_product_sku_enabled', 'sv_remove_product_page_skus' );
 
+  global $product; 
+
  function action_woocommerce_before_main_content() {
 	$smallKcal = get_field('small_portion_kcal', $product->ID);
 	$smallProtein = get_field('small_portion_protein', $product->ID);
 	$largeKcal = get_field('large_portion_kcal', $product->ID);
   $largeProtein = get_field('large_portion_protein', $product->ID);
 
-	echo '<div class="macro-container">';
-	echo '<div class="macro-container__box">';
-	echo '<h4 class="macro-container__box-title">Kcal</h4>';
-	echo '<div class="macro-container__box-container">';
-  echo '<h4 class="macro-container__box-macro">'. strstr($largeKcal, '.', true) . ' / ' . strstr($smallKcal, '.', true) .'</h4>';
-	echo '</div>';
-	echo '</div>';
-	echo '<div class="macro-container__box">';
-	echo '<h4 class="macro-container__box-title">Protein</h4>';
-	echo '<div class="macro-container__box-container">';
-  echo '<h4 class="macro-container__box-macro small-protein">'. strstr($largeProtein, '.', true) . ' / ' . strstr($smallProtein, '.', true) .'</h4>';
-	echo '</div>';
-	echo '</div>';
-  echo '</div>';
+    echo '<div class="macro-container">';
+    echo '<div class="macro-container__box kcal">';
+    echo '<h4 class="macro-container__box-title">Kcal</h4>';
+    echo '<div class="macro-container__box-container">';
+    echo '<h4 class="macro-container__box-macro large-cal">'. $largeKcal .'</h4>';
+    echo '<h4 class="macro-container__box-macro small-cal">'. $smallKcal .'</h4>';
+    echo '</div>';
+    echo '</div>';
+    echo '<div class="macro-container__box protein">';
+    echo '<h4 class="macro-container__box-title">Protein</h4>';
+    echo '<div class="macro-container__box-container">';
+    echo '<h4 class="macro-container__box-macro small-protein large-protein">'. $largeProtein .'</h4>';
+    echo '<h4 class="macro-container__box-macro small-protein small-protein">'. $smallProtein .'</h4>';
+    echo '</div>';
+    echo '</div>';
+    echo '</div>';
 };
 
 add_action( 'woocommerce_single_product_summary', 'action_woocommerce_before_main_content', 10, 2 );
