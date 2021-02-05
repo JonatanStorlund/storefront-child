2df016c6f6608ae303364cd20510917ab179f9cd
Jonatan <storlundjonatan@gmail.com>
2021-02-05 10:26:40 +0200

    fix checkout layout

--- assets/scripts/translate.js
+++ assets/scripts/translate.js
@@ -1,25 +1,21 @@
 jQuery(document).ready(function ($) {
   $("[lang='sv-SE'] button#place_order").html('Slutför köp');
-  $("[lang='sv-SE'] .woocommerce-shipping-totals th").html('Välj leverans / hämtningsplats');
-  $("[lang='fi'] .woocommerce-shipping-totals th").html('Valitse toimitus / noutopaikka ');
+  $("[lang='sv-SE'] .woocommerce-shipping-totals th").html('Välj leverans eller hämtar själv');
+  $("[lang='fi'] .woocommerce-shipping-totals th").html('Valitse toimitus tai noutopaikka ');
   $("[lang='sv-SE'] .woocommerce-privacy-policy-text").html("<div class='woocommerce-privacy-policy-text'><p>Dina personuppgifter kommer att användas för att behandla din beställning, stödja din upplevelse på hela denna webbplats och för andra ändamål som beskrivs i vår <a href=" + document.location.origin + "/integritetspolicy>integritetspolicy</a></p></div>");
   $("[lang='fi'] .woocommerce-privacy-policy-text").html("<div class='woocommerce-privacy-policy-text'><p>Henkilötietojasi käytetään tilauksesi käsittelyyn, kokemuksen tukemiseen tällä verkkosivustolla ja muihin tarkoituksiin, jotka on kuvattu integritets-ohjelmassa. <a href=" + document.location.origin + "/integritetspolicy>integritetspolicy</a></p></div>");
-  $('[lang="fi"] #shipping_method_0 [value="flat_rate:6"]').text("Kotiinkuljetus: 6.90€");
-  $('[lang="fi"] #shipping_method_0 [value="local_pickup:7"]').text("Noudan Svenska Gårdenin takapihalta, Pietarsaari - 0€");
-  $('[lang="fi"] #shipping_method_0 [value="local_pickup:12"]').text("Noudan GYM365, Vöyri - 0€");
-  $('[lang="fi"] #shipping_method_0 [value="free_shipping:11"]').text("Ilmainen kotiin- /toimisto kuljetus - 0€");
   $('[lang="sv-SE"] .swatch-large').text("Stor");
   $('[lang="fi"] .swatch-large').text("Iso");
   $('[lang="fi"] .swatch-normal').text("Tavallinen");
+  $('[lang="fi"] .shipping-pickup-store th strong').text("Valitse noutopaikka");
+  $('[lang="sv-SE"] .shipping-pickup-store th strong').text("Välj hämtningsplats");
 
   $('body').on('updated_checkout', function () {
-    $('[lang="fi"] #shipping_method_0 [value="flat_rate:6"]').text("Kotiinkuljetus: 6.90€");
-    $('[lang="fi"] #shipping_method_0 [value="local_pickup:7"]').text("Noudan Svenska Gårdenin takapihalta, Pietarsaari - 0€");
-    $('[lang="fi"] #shipping_method_0 [value="local_pickup:12"]').text("Noudan GYM365, Vöyri - 0€");
-    $('[lang="fi"] #shipping_method_0 [value="free_shipping:11"]').text("Ilmainen kotiin- /toimistokuljetus - 0€");
+    $('[lang="fi"] .shipping-pickup-store th strong').text("Valitse noutopaikka");
+    $('[lang="sv-SE"] .shipping-pickup-store th strong').text("Välj hämtningsplats");
     $("[lang='sv-SE'] button#place_order").html('Slutför köp');
-    $("[lang='sv-SE'] .woocommerce-shipping-totals th").html('Välj leverans / hämtningsplats');
-    $("[lang='fi'] .woocommerce-shipping-totals th").html('Valitse toimitus / noutopaikka ');
+    $("[lang='sv-SE'] .woocommerce-shipping-totals th").html('Välj leverans eller hämtar själv');
+    $("[lang='fi'] .woocommerce-shipping-totals th").html('Valitse toimitus tai noutopaikka ');
     $("[lang='sv-SE'] .woocommerce-privacy-policy-text").html("<div class='woocommerce-privacy-policy-text'><p>Dina personuppgifter kommer att användas för att behandla din beställning, stödja din upplevelse på hela denna webbplats och för andra ändamål som beskrivs i vår <a href=" + document.location.origin + "/integritetspolicy>integritetspolicy</a></p></div>");
     $("[lang='fi'] .woocommerce-privacy-policy-text").html("<div class='woocommerce-privacy-policy-text'><p>Henkilötietojasi käytetään tilauksesi käsittelyyn, kokemuksen tukemiseen tällä verkkosivustolla ja muihin tarkoituksiin, jotka on kuvattu integritets-ohjelmassa. <a href=" + document.location.origin + "/tietosuojaseloste >tietosuojaseloste </a></p></div>");
   });
