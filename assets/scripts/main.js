9785fa75e672a1706b572348a15d88f529ef8120
Jonatan <storlundjonatan@gmail.com>
2021-02-09 19:24:44 +0200

    minor style changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -118,7 +118,6 @@ jQuery(document).ready(function ($) {
   $("[lang='sv-SE'] .woocommerce-billing-fields h3").html('Kontrollera din order, välj leverans- /hämtnings alternativ och fyll i dina uppgifter');
   $("[lang='fi'] .woocommerce-billing-fields h3").html('Tarkista tilaus, valitse toimitus- /nouto vaihtoehdoista ja täytä tietojasi');
   $("[lang='sv-SE'] button#place_order").html('Slutför köp');
-  $("[lang='fi'] button#place_order").html('Osta');
   $("[lang='sv-SE'] .woocommerce-shipping-totals th").html('Välj leverans eller hämtar');
   $("[lang='fi'] .woocommerce-shipping-totals th").html('Valitse toimitus tai nouto ');
   $("[lang='sv-SE'] .woocommerce-privacy-policy-text").html("<div class='woocommerce-privacy-policy-text'><p>Dina personuppgifter kommer att användas för att behandla din beställning, stödja din upplevelse på hela denna webbplats och för andra ändamål som beskrivs i vår <a href=" + document.location.origin + "/integritetspolicy>integritetspolicy</a></p></div>");
@@ -130,12 +129,15 @@ jQuery(document).ready(function ($) {
   $('[lang="sv-SE"] .shipping-pickup-store th strong').text("Välj hämtningsplats");
   $('[lang="fi"] .woocommerce-cart .coupon label').text("Onko sinulla kuponki? Voit lisää sen tähän");
   $('[lang="sv-SE"] .woocommerce-cart .coupon label').text("Har du en rabattkod? Fyll i den här");
+  $('[lang="fi"] .woocommerce-checkout .wpll-pickup-null').text("valitse noutopaikka");
+  $('[lang="sv-SE"].woocommerce-checkout .wpll-pickup-null').text("välj en upphämtningsplats");
 
   $('body').on('updated_checkout', function () {
+    $('[lang="fi"] .woocommerce-checkout .wpll-pickup-null').text("valitse noutopaikka");
+    $('[lang="sv-SE"] .woocommerce-checkout .wpll-pickup-null').text("välj en upphämtningsplats");
     $('[lang="fi"] .shipping-pickup-store th strong').text("Valitse noutopaikka");
     $('[lang="sv-SE"] .shipping-pickup-store th strong').text("Välj hämtningsplats");
     $("[lang='sv-SE'] button#place_order").html('Slutför köp');
-    $("[lang='fi'] button#place_order").html('Osta');
     $("[lang='sv-SE'] .woocommerce-shipping-totals th").html('Välj leverans eller hämtar');
     $("[lang='fi'] .woocommerce-shipping-totals th").html('Valitse toimitus tai nouto ');
     $("[lang='sv-SE'] .woocommerce-privacy-policy-text").html("<div class='woocommerce-privacy-policy-text'><p>Dina personuppgifter kommer att användas för att behandla din beställning, stödja din upplevelse på hela denna webbplats och för andra ändamål som beskrivs i vår <a href=" + document.location.origin + "/integritetspolicy>integritetspolicy</a></p></div>");
